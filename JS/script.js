6519f434db34ec17733a8f263481fdc4a4242e43
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-01 20:24:58 +0600

    Operator Precedance

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,14 @@
-// MATH OPERATOR
-const now = 2030;
-const ageLindos = now - 1991;
+// PRECEDANCE OPERATOR
+const ageLindos = 2030 - 1992;
 const ageSara = 2030 - 2019;
 console.log(ageLindos, ageSara);
-console.log(ageLindos * 2, ageSara / 10, 2 ** 3);
-// 2 ** 3 means to to the power of 3 = 2 * 2 * 2
 
-const firstName = 'Lindos';
-const lastName = 'Mahadi';
-console.log(firstName + " " + lastName);
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
 
-// ASSIGNMENT OPERATOR
-let x = 10 + 5; // 15
-x += 10; // x = x+ 10;
-x *= 4; // x = x * 4 = 100
-x++; // 101
-x--; //100
-x--; //99
-console.log(x);
+const averageAge = (ageLindos + ageSara) / 2
+console.log(ageLindos, ageSara, averageAge);
 
-// comparison operators
-console.log(ageLindos > ageSara); // >, <, >=, <=
-console.log(ageSara >= 18);
-const isFullAge = ageSara >= 18;
-console.log(isFullAge);
-console.log(now - 1991 > now - 2019);
 
 
