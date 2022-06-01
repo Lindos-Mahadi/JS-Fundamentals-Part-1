635bd908203c21c863d39c063cfacdd1bb269818
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-01 18:35:53 +0600

    Basic operator

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,29 @@
-// LET
-let age = 30;
-age = 20;
-console.log(age);
+// MATH OPERATOR
+const now = 2030;
+const ageLindos = now - 1991;
+const ageSara = 2030 - 2019;
+console.log(ageLindos, ageSara);
+console.log(ageLindos * 2, ageSara / 10, 2 ** 3);
+// 2 ** 3 means to to the power of 3 = 2 * 2 * 2
 
-// CONST is not changable because its constant
-const birthYear = 1990;
-// birthYear = 1980;
-console.log(birthYear);
+const firstName = 'Lindos';
+const lastName = 'Mahadi';
+console.log(firstName + " " + lastName);
 
-// VAR
-var job = "programming";
-job = "programmer";
-console.log(job);
+// ASSIGNMENT OPERATOR
+let x = 10 + 5; // 15
+x += 10; // x = x+ 10;
+x *= 4; // x = x * 4 = 100
+x++; // 101
+x--; //100
+x--; //99
+console.log(x);
+
+// comparison operators
+console.log(ageLindos > ageSara); // >, <, >=, <=
+console.log(ageSara >= 18);
+const isFullAge = ageSara >= 18;
+console.log(isFullAge);
+console.log(now - 1991 > now - 2019);
+
+
