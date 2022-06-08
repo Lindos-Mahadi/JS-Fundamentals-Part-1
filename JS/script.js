f06dabbd76be3c47e4f60133f2b3b22e81b85cca
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-08 11:59:48 +0600

    REVIEWING FUNCTIONS

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,22 @@
-// FUNCTIONS CALLING OTHER FUNCTIONS
+// REVIEWING FUNCTIONS
 
-function cutFruitPieaces(fruit) {
-    return fruit * 4;
+const calAge = function (birthYear) {
+    return 2037 - birthYear;
 }
 
-function fruitProcessor(apples, oranges) {
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calAge(birthYear);
+    const retirement = 65 - age;
 
-    const applesPieaces = cutFruitPieaces(apples);
-    const orangesPieaces = cutFruitPieaces(oranges);
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has laready retired 💜`);
+        return -1;
+    }
 
-    const juice = `Juice with ${applesPieaces} piece of apple and ${orangesPieaces} pieace of orange`;
-    return juice;
 }
-console.log(fruitProcessor(2, 3));
+
+console.log(yearsUntilRetirement(1994, 'lindos'));
+console.log(yearsUntilRetirement(1954, 'lindos'));
