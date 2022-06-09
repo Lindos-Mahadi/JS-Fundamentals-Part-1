a3ed6b661198ba7f3b09adc7e6bc0c87086883aa
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-09 18:37:06 +0600

    CODDING CHALANGE ONE PART 2

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,49 @@
-// REVIEWING FUNCTIONS
+// CODDING CHALANGE ONE PART 2
 
-const calAge = function (birthYear) {
-    return 2037 - birthYear;
-}
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-    const age = calAge(birthYear);
-    const retirement = 65 - age;
+// let scoreDolphines = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphines, scoreKoalas);
 
-    if (retirement > 0) {
-        console.log(`${firstName} retires in ${retirement} years`);
-        return retirement;
+// const checkWinner = function (avgDolhins, avgKoalas) {
+//     if (avgDolhins >= 2 * avgKoalas) {
+//         console.log(`Dolphines win 🏆 ${avgDolhins} vs. ${avgKoalas}`);
+//     } else if (avgDolhins >= 2 * avgKoalas) {
+//         console.log(`Koalas win 🏆 ${avgKoalas} vs. ${avgDolhins}`);
+//     }
+//     else {
+//         console.log('No team wins');
+//     }
+// }
+// checkWinner(scoreDolphines, scoreKoalas);
+// checkWinner(555, 111);
+
+// scoreDolphines = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphines, scoreKoalas);
+// checkWinner(scoreDolphines, scoreKoalas);
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(44, 23, 71));
+
+let avgDolhins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+console.log(avgDolhins, avgKoalas);
+
+const checkWinner = function (avgDolhins, avgKoalas) {
+    if (avgDolhins > 2 * avgKoalas) {
+        console.log(`Dolphines win ${avgDolhins} 🏆 vs. Koalas ${avgKoalas}`);
+    } else if (avgDolhins > 2 * avgKoalas) {
+        console.log(`Koalas win ${avgKoalas} 🏆 vs. Dolphines ${avgDolhins}`);
     } else {
-        console.log(`${firstName} has laready retired 💜`);
-        return -1;
+        console.log('No team winer');
     }
-
 }
+checkWinner(avgDolhins, avgKoalas);
 
-console.log(yearsUntilRetirement(1994, 'lindos'));
-console.log(yearsUntilRetirement(1954, 'lindos'));
+checkWinner(555, 111);
+avgDolhins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+console.log(avgDolhins, avgKoalas);
+checkWinner(avgDolhins, avgKoalas);
