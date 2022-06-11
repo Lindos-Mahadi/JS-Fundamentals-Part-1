1bbb78a8ff39fccbe284deb25f6ed860adc8abe4
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-11 13:34:40 +0600

    LOOPING BACKWORDS AND LOOPS IN LOOPS

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,31 @@
-// FOR LOOP KEEPS RUNNING WHILE CONDITION IS TRUE
+// LOOPING BACKWORDS AND LOOPS IN LOOPS
 
-for (let index = 1; index <= 10; index++) {
-    const element = index;
-    console.log(`Lifting weights repetetion ${element}`);
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven'],
+//     true,
+//     false
+// ];
+
+// // 0, 1,.....4
+// // 4, 5,..., 0
+
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//     console.log(i, jonas[i], typeof jonas[i]);
+// }
+
+// LOOPS IN LOOP
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-------Starting exercise ${exercise}`);
+
+    // for (let rep = 1; rep < 6; rep++) {
+    //     console.log(`Lifting weight repetation ${rep} 🥈`);
+    // }
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: 🧯 Lifting weight repetation ${rep} 🥈`);
+    }
 }
-
