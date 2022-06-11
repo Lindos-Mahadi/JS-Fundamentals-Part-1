4556cb6fd9c73bda2b56bfeecea3222d52d9c306
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-11 11:03:51 +0600

    CODING CHALLENGE Three Part Two

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,30 @@
-// OBJECT METHODS
+// CODING CHALLENGE Three Part Two
 
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYear: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriverLicense: true,
+const Mark = {
+    fullName: 'Mark Killer',
+    mass: 78,
+    height: 1.69,
 
-    // calcAge: function (birthYear) {
-    //     return 2022 - birthYear;
-    // }
-
-    // calcAge: function () {
-    //     console.log(this);
-    //     return 2022 - this.birthYear;
-    // }
-
-    calcAge: function () {
-        // console.log(this);
-        this.age = 2022 - this.birthYear;
-        return this.age;
-    },
-
-    getSummary: function () {
-        return `${this.firstName} is a ${this.calcAge()}-years old ${this.job}, and he has ${this.hasDriverLicense ? 'a' : 'no'} driver's license.`;
-
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
     }
-};
-// console.log(jonas.calcAge(1991));
-// console.log(jonas['calcAge'](1991));
+}
+const John = {
+    fullName: 'John Smith',
+    mass: 79,
+    height: 1.69,
 
-// console.log(jonas.calcAge());
-
-console.log(jonas.calcAge());
-console.log(jonas.age);
-console.log(jonas.age);
-
-// Challenge
-// "Jonas is a 46-year old teacher, and he has a driver's license"
-console.log(jonas.getSummary());
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+Mark.calcBMI();
+John.calcBMI();
+console.log(Mark.bmi, John.bmi);
+if (Mark.bmi > John.bmi) {
+    console.log(`${Mark.fullName} is ${Mark.bmi} higher then John ${John.bmi}`);
+} else if (John.bmi > Mark.bmi) {
+    console.log(`${John.fullName} is ${John.bmi} higher then John ${Mark.bmi}`);
+}
