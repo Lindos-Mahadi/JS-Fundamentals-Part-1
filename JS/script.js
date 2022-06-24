788eb9d2d1845a90f0aa55adac2b05a31e28edba
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-24 11:24:50 +0600

    Handling Click Events

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,22 @@
-// Remember we are gonna use strict mode in all scripts now !
-
 'use strict';
 
-// PROBLEM
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number';
 
-// We work for a company building a smart home thermomiter. Our most recent task is this : "Given an array of temperatures of one day, calculate the temperature amplitude. keep in mind theat sometimes there might be a sensor error"
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
 
-// Test data:
-// § Data 1: [3, -2, -6 , 'error', 9, 13, 17, 15, 14, 9, 5];
+document.querySelector('.guess').value = 10;
+console.log(document.querySelector('.guess').value);
+*/
 
-// 1) Understanding the problem
-//---What is temp amplitude? answer: difference between highest and lowest temp
-//---What is the X days? answer: index + 1How to compute max and min temperatures?
-//---Whats a sensor error? and what to do?
+// REAL PROGRAM IS GOING TO START
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-// 2) Breaking Up into sub-problem
-//---How to ignore errors?
-//---Find max value in temp array
-//---Find min value in temp array
-//---Substract min from max (amplitude and return it)
-
-const dataOne = [3, -2, -6, 'error', 9, 13, 17, 15, 14, 9, 5];
-
-console.log(`${dataOne} `);
-
-const calcTempAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
-
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+  if (!guess) {
+    document.querySelector('.message').textContent = '🎉 No Number';
   }
-  console.log(max, min);
-  return max - min;
-};
-
-const amplitude = calcTempAmplitude(dataOne);
-console.log(amplitude);
+});
