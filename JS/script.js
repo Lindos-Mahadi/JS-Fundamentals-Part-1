ab415c2c06ebc440651254c20812255307ac3a66
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-26 20:03:49 +0600

    Working with Classes & Simplyfing and Refactoring Code

--- JS/script.js
+++ JS/script.js
@@ -4,9 +4,46 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.closeModal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+
+//     // CSS STYLE
+//     // modal.style.display = 'block';
+//   });
+
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+// SIMPLYFING CODE
+
+// CLOSE FUNCTION AND USING FUNCTION NAME
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// CLOSE FUNCTION AND USING FUNCTION NAME
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
