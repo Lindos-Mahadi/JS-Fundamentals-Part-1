2a4087c4dc549ba747b7a9dce980df0eb230ec00
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-27 17:09:09 +0600

    Resseting the Game

--- JS/script.js
+++ JS/script.js
@@ -16,16 +16,30 @@ const btnHNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 // ROLLING DICE FUNCTIONALITY
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 // CREATE FUNCTION FOR CLEAN CODE
 const switchPlayer = function () {
@@ -87,3 +101,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// RESSTING THE GAME
+btnHNew.addEventListener('click', init);
