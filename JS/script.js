10104e13b7608e523fb96eb067d5f4fe79a05a67
Lindos Mahadi <lindosmahadi@gmail.com>
2022-06-27 12:01:57 +0600

    Switching the active player

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,9 @@
 // PROJECT #3
 // SELECTING ELEMENTS
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -19,7 +22,9 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 // ROLLING DICE FUNCTIONALITY
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -33,8 +38,17 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    // CHANGE int dynamic
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    // CSS STYLE
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
