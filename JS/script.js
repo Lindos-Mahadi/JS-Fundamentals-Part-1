8ab3f36726b0503cc819dc43f2ed56ee45e3101c
Lindos Mahadi <lindosmahadi@gmail.com>
2022-07-13 13:18:53 +0600

    VARIABLE DECLAION

--- JS/script.js
+++ JS/script.js
@@ -1,108 +1,12 @@
 'use strict';
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  // ES6 enhanced object literals openingHours,
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliver to ${address} at ${time}`
-    );
-  },
-};
-
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Via del Sol, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-});
-
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
-
-// Destructing Objects
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
-
-// Default value
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
-
-// Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 10, b: 20, c: 30 };
-({ a, b } = obj);
-console.log(a, b);
-
-// Nested Object
-const {
-  fri: { open, close },
-} = openingHours;
-console.log(open, close);
-
-/////////////////////////////////////////////////////
-// Destructiong Arrays
-// const arr = [2, 3, 4, 5];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
-
-// const [x, y, z] = arr;
-// console.log(x, y, z);
-// console.log(arr);
-
-// let [main, , secondary] = restaurant.categories;
-// console.log(main, secondary);
-
-// // switching variables
-// // const temp = main;
-// // main = secondary;
-// // secondary = temp;
-
-// [main, secondary] = [secondary, main];
-// console.log(main, secondary);
-
-// // Receive 2 return values from a function
-// console.log(restaurant.order(2, 0));
-// const [starter, mainCourse] = restaurant.order(2, 0);
-// console.log(starter, mainCourse);
-
-// // Nested destructuring
-// const nested = [2, 4, [5, 6]];
-// // const [i, j] = nested;
-// const [i, j, [k, l]] = nested;
-// console.log(i, j, l);
-
-// // Default values
-// // const [p, q, r] = [8, 9];
-// // console.log(p, q, r);
-
-// const [p = 1, q = 1, r = 1] = [8, 9];
-// console.log(p, q, r);
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
